c87e18b4da1add0a7a759f879cdff0faf66d1f6c
Koima-Sam <sam17koima@gmail.com>
2022-09-03 17:13:17 +0300

    commit tree 03d2b286105cc474339bedbf97c844ab34400acf

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,8 @@
 class Movie < ActiveRecord::Base
-    
+   has_many :roles
+   has_many :actors, through: :roles
+
+   def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,11 @@
 class Role < ActiveRecord::Base
-    
+    belongs_to :movie
+    belongs_to :actor
+
+    def credit
+        "#{self.character_name} acted by #{self.actor.name}"
+        # self.all.map do |t|
+        #     puts "#{t.character_name} acted by #{t.actor.name}"
+        # end
+    end
 end
